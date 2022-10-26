a664cfa454cd4fc114080d95ef0d67fff40ebd5f
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-10-26 21:51:48 +0000

    [Add] ヘッダー調整

--- db/migrate/20221019193546_create_users.rb
+++ db/migrate/20221019193546_create_users.rb
@@ -5,11 +5,11 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string  :uid
       t.string  :nickname
       t.string  :image
-      t.string  :name 
+      t.string  :name
       t.integer :twitter_id       , null: false
       t.integer :acquisition_point, null: false
       t.string  :introdaction     , null: false
-      
+
       t.timestamps
     end
   end
