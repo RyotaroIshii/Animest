75c8167fab2600c7b9fe0e1e273fed78a6f923fc
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-10-27 21:52:39 +0000

    [Add] トップページ作成途中

--- db/migrate/20221019193546_create_users.rb
+++ db/migrate/20221019193546_create_users.rb
@@ -6,6 +6,8 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string  :nickname
       t.string  :image
       t.string  :name
+      t.integer :acquisition_point, null: false, default: 0
+      t.string  :introdaction     , null: false
 
       t.timestamps
     end
