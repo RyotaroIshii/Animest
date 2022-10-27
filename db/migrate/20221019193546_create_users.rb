9841b5e2357fb52d6efa44017a6a0cc6fd665896
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-10-27 01:43:07 +0000

    [Add] 修正

--- db/migrate/20221019193546_create_users.rb
+++ db/migrate/20221019193546_create_users.rb
@@ -6,9 +6,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string  :nickname
       t.string  :image
       t.string  :name
-      t.integer :twitter_id       , null: false
-      t.integer :acquisition_point, null: false
-      t.string  :introdaction     , null: false
 
       t.timestamps
     end
