1cf8ed921316d56cccf4cddf970cb05d91561008
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2023-03-31 17:16:58 +0000

    修正

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -31,7 +31,6 @@ class AnimesController < ApplicationController
   def index
     @animes = result.data.search_works.edges.map(&:node)
     @animes = Kaminari.paginate_array(@animes).page(params[:page]).per(100)
-    @years = @animes.map(&:season_year).uniq.sort
   end
 
   def year
--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  
 
 
 
